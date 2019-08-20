c4768b25a554b205b2ebed136a9e7b49357d41f2
dinesh196 <chavandinesh196@gmail.com>
2019-08-20 09:48:06 +0530

    Enable django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self,email,name,password = None):
+    def create_user(self,email, name,password=None):
         """Create a new User profile"""
         if not email:
             raise ValueError('User must have a email address')
@@ -17,24 +17,25 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self,email,name,password):
+    def create_superuser(self, email, name, password):
         """Create a new SuperUser profile"""
-        user = self.create_user(email,name,password)
+        user = self.create_user(email, name, password)
+
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
         return user
 
+
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
-
-    email = models.EmailField(max_length=255,unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -42,16 +43,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve full name of user"""
+        """Retrieve full name for user"""
         return self.name
-    
+
     def get_short_name(self):
         """Retrieve short name of user"""
         return self.name
 
-    def __str__(self):
-        """Retrun string representation of user"""
+    def _str_(self):
+        """Return string representation of user"""
         return self.email
